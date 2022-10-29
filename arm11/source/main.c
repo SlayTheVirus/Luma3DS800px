f9c65958d3c57735b9a2074ceeb84a6e3a8e65da
bscarell <bscarell@gmail.com>
2022-10-29 11:16:52 -0400

    Update main.c

--- arm11/source/main.c
+++ arm11/source/main.c
@@ -35,7 +35,7 @@ static enum {
 	MODE_WIDE = 2,
 } gfxTopMode;
 
-#include "/opt/devkitpro/devkitarm/include/3ds/gfx.h"
+#include "/devkitpro/devkitarm/include/3ds/gfx.h"
 #include "types.h"
 #include "memory.h"
 
